6a65a53af1fa05a418cc609cea87de74cf7b9075
Matthew Shao <me@matshao.com>
2016-07-20 20:51:47 +0800

    Add script.py with basic structure.

---
+++ test/Server/test_scriptBase.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestScriptBase(TestCase):
+    def test_toggle_enable(self):
+        self.fail()
+
+    def test_set_invoke_rule(self):
+        self.fail()
+
+    def test_invoke_check(self):
+        self.fail()
+
+    def test_send_task(self):
+        self.fail()
