886359fa9621038ddd758a2aeab1f75f8526d3d9
Matthew Shao <me@matshao.com>
2016-07-25 21:21:26 +0800

    Add test for JobPool.

--- test/Server/test_job.py
+++ test/Server/test_job.py
@@ -1,5 +1,21 @@
 from unittest import TestCase
+from Server.job import JobPool
 
 
 class TestJobPool(TestCase):
-    pass
+
+    def setUp(self):
+        self.pool = JobPool(10)
+
+    def test_add(self):
+        for i in range(10):
+            self.pool.add(i)
+        self.pool.add(999)
+        assert self.pool[0] == 1
+        assert self.pool[9] == 999
+
+    def test_iter(self):
+        for i in range(10):
+            self.pool.add(i)
+        for i in self.pool:
+            pass
