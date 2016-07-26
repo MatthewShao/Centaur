0eba9da7d6cc6f3345a245c5a69cd339b029d88f
Matthew Shao <me@matshao.com>
2016-07-26 21:19:59 +0800

    Idea about how to code the ScriptTask class.

--- Worker/task.py
+++ Worker/task.py
@@ -1,5 +1,20 @@
-from celery import Celery
+from celery import Celery, Task
 
 celery = Celery('task')
 celery.config_from_object('config')
 
+def load_scripts():
+    # 1. List scripts on Server.
+    # 2. Download scripts and save in the form of dict
+    pass
+
+
+def run(self, flow):
+    print flow
+
+# load task in this way. Create the task class dynamically
+d = {'a':None, 'b':None, 'c':None}
+for k in d.iterkeys():
+    d[k] = type(k, (Task,), {'name':'task.'+k, 'run':run})
+
+print d
