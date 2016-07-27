8424de72c1ca6dadce5b0b58e0c8ba1415a7b6df
Matthew Shao <me@matshao.com>
2016-07-27 22:05:32 +0800

    Add config item for Worker.

--- Worker/config.py
+++ Worker/config.py
@@ -6,3 +6,6 @@ BROKER_URL = "amqp://guest:guest@rabbitmq.t0.daoapp.io:61539//"
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
+
+SERVER = "http://127.0.0.1:5000"
+TASK_TIMEOUT = 30
