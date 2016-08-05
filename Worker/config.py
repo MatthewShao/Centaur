f145c384064b4a723903f2829025c86fb34318da
Matthew Shao <me@matshao.com>
2016-08-05 15:24:12 +0800

    Turn on backend storage.

--- Worker/config.py
+++ Worker/config.py
@@ -1,8 +1,8 @@
 BROKER_URL = "amqp://guest:guest@rabbitmq.t0.daoapp.io:61539//"
-# CELERY_RESULT_BACKEND = 'mongodb//localhost:27017/'
-# CELERY_MONGODB_BACKEND_SETTINGS = {
-#     'database':'centaur'
-# }
+CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/'
+CELERY_MONGODB_BACKEND_SETTINGS = {
+    'database':'centaur'
+}
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
