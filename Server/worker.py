9e9be6aa286a0b8a34861cc308966bb4f905fc97
Matthew Shao <me@matshao.com>
2016-09-07 23:06:08 +0800

    Add InspectWorker and ManageWorker api.

--- Server/worker.py
+++ Server/worker.py
@@ -4,7 +4,9 @@ from Server.lib.celery_ import celery
 
 def init_parse():
     parser = reqparse.RequestParser()
-    parser.add_argument("task", type=str)
+    parser.add_argument('task', type=str)
+    parser.add_argument('action', type=str, location='json')
+    parser.add_argument('dest', type=list, location='json')
     return parser
 
 worker_bp = Blueprint('worker', __name__)
@@ -19,7 +21,7 @@ class WorkerList(Resource):
         return jsonify(inspect.registered())
 
 
-class Worker(Resource):
+class InspectWorker(Resource):
 
     def get(self, name):
         args = parser.parse_args()
@@ -52,11 +54,42 @@ class Worker(Resource):
             response.status_code = 400
         return response
 
-    def post(self, name):
+class ManageWorker(Resource):
+
+    def post(self):
         # TODO provide remote control operations, including
         # cancel consumers, pool restart, shutdown, purge
-        pass
+        args = parser.parse_args()
+        dest = args['dest']
+        # check if dest worker registered
+        if args['action'] == 'purge':
+            count = celery.control.purge()
+            return jsonify({"msg": "{} tasks have been discarded".format(count)})
 
+        registered = celery.control.inspect(dest).registered()
+        if not registered:
+            response = make_response(jsonify({
+                "msg": "Invalid woker name appears."
+            }))
+            response.status_code = 400
+            return response
+
+        if args['action'] == 'cancel_consumer':
+            return celery.control.cancel_consumer(args['task'], reply=True, destination=args['dest'])
+        elif args['action'] == 'pool_restart':
+            return celery.control.broadcast('pool_restart', arguments={'reload': True}, reply=True)
+        elif args['action'] == 'shutdown':
+            celery.control.broadcast('shutdown', destination=args['dest'])
+            return jsonify({
+                "msg": "Shutdown messages have sent to {}".args['dest']
+            })
+        else:
+            response = make_response(jsonify({
+                "msg": "Invalid action."
+            }))
+            response.status_code = 400
+            return response
 
 worker_api.add_resource(WorkerList, "/list/workers")
-worker_api.add_resource(Worker, "/worker/<string:name>")
+worker_api.add_resource(InspectWorker, "/worker/<string:name>")
+worker_api.add_resource(ManageWorker, "/worker")
